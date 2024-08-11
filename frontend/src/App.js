a1c1c723ee41a3adfc4e390189ac1d4dce5eee9f
Kurubasaipraveen <ksaipraveen6@gmail.com>
2024-08-11 14:00:39 +0530

    first

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import LivingRoomDesigns from './components/LivingRoomDesigns';
-
+import './App.css'
 
 function App() {
   return (
